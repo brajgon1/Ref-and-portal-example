70b8f59185ef4203395a68849ae9c81c5fc32f0f
Brandon Gonzalez <brajgon@gmail.com>
2024-05-16 20:06:37 -0600

    intervals, new states, etc

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,20 +1,33 @@
-import { forwardRef } from 'react'
- 
-const ResultModal = forwardRef (function ResultModal({ result, targetTime }, ref) {
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime }, ref) {
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return (
-    <dialog ref={ref} className="result-modal">
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You Lost!</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with <strong>{formattedRemainingTime}s left.</strong>
       </p>
       <form method="dialog">
         <button>Close</button>
       </form>
     </dialog>
   );
-})
+});
 
 export default ResultModal;
